40c13b6ad4b91b8a4ba259135162c3add5c58ccd
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2014-02-14 16:06:31 +0000

    Remove sessions

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
-app.use(express.session());
 app.use(partials());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
